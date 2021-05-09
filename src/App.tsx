e332ed463cebf0683e6af0a113c9f760d2c60d12
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-10 00:16:15 +0900

    컴포넌트 사용법 익히기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import {useEffect, useState} from 'react';
 import './App.css';
+import ImportComponent from './R003_ImportComponent'
 
 function App() {
   // const [message, setMessage] = useState("");
@@ -18,6 +19,7 @@ function App() {
         <h1>Start React 200!</h1>
         <p>HTML 적용하기</p>
         <p>CSS 적용하기</p>
+        <ImportComponent></ImportComponent>
     </div>
   );
 }
