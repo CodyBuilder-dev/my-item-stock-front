b4ba9e41c2720de0d517317b2b517f0f215f169b
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-26 16:28:56 +0900

    React-Router적용

---
+++ src/Chapter3/R089_ReactRouter2.tsx
@@ -0,0 +1,13 @@
+import React, {useEffect} from "react";
+
+function R089_ReactRouter2() {
+    return (
+        <>
+           <h1>path="/reactRouter2"</h1>
+           <h3>R089_reactRouter</h3>
+        </>
+    )
+}
+
+
+export default R089_ReactRouter2;
