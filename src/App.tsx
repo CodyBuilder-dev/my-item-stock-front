4b2489cf1e76a1308cab2cfdf505ac632f6123c4
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-26 18:04:22 +0900

    Recharts 의존성 설치 및 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
 import {connect} from "react-redux";
-import StrAddButton from './StrAddButton';
+import {Route} from 'react-router-dom';
 
-import './App.css';
+// import './App.css';
+import './css/new.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
 // import Chapter1 from "./Chapter1";
 // import Chapter2 from "./Chapter2"
 // import Chapter3 from "./Chapter3"
+// import StrAddButton from './StrAddButton';
+
+import floatingPopulationList from "./components/floating_population/floatingPopulationList";
+import rechartsSimpleLineChart from "./components/floating_population/rechartsSimpleLineChart";
+
 
 function App(props: any) {
   return (
-    <div>
-    {/*<div className="App">*/}
+    <div className="App">
         <h1>Start React 200!</h1>
           {/*<Chapter1/>*/}
           {/*<Chapter2/>*/}
-          {/*<Chapter3/>*/}
+          {/*<Chapter3/> */}
+
         {/*<span>{props.store.getState().data.str}</span><br/>*/}
         {/*<StrAddButton store={props.store}/>*/}
 
-        <span>{props.str}</span><br/>
-        <StrAddButton AppProp={"200"}/>
+        {/*<span>{props.str}</span><br/> */}
+        {/*<StrAddButton AppProp={"200"}/> */}
+        <Route path="/floatingPopulationList" component={floatingPopulationList} />
+        <Route path="/rechartsSimpleLineChart" component={rechartsSimpleLineChart} />
     </div>
   );
 }
 
-let mapStateToProps = (state: any, props: any) => {
-    console.log("Props from index.js : " + props.indexProp)
-    return {
-        str: state.data.str
-    }
-}
+// let mapStateToProps = (state: any, props: any) => {
+//     console.log("Props from index.js : " + props.indexProp)
+//     return {
+//         str: state.data.str
+//     }
+// }
 
-App = connect(mapStateToProps,null)(App);
+// App = connect(mapStateToProps,null)(App);
+// export default connect(mapStateToProps,null)(App);
+
 export default App;
-
