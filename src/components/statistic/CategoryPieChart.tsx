6dd8ebf03d7c3a53f3b6ce3ee1c1ca67b9cd2431
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-06-02 04:05:30 +0900

    백엔드 통신 및 데이터 받아오기 구현

--- src/components/statistic/CategoryPieChart.tsx
+++ src/components/statistic/CategoryPieChart.tsx
@@ -1,47 +1,73 @@
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import React from "react";
+import { PieChart, Pie, Cell } from "recharts";
+import axios from "axios";
 
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
+// const data = [
+//     { name: "Group A", value: 400 },
+//     { name: "Group B", value: 300 },
+//     { name: "Group C", value: 300 },
+//     { name: "Group D", value: 300 }
+// ];
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const url = "/api/item-list";
+let data:any[] = [];
+axios.post(url,{
+    "email":"gogo@naver.com"
+})
+    .then( response => {
+        console.log(response.data);
+        data = response.data.itemList.map((item: any) => (
+            {name:item.name, value:item.amount}
+        ));
+        console.log(data);
+    })
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+const renderCustomizedLabel = ({
+                                   cx,
+                                   cy,
+                                   midAngle,
+                                   innerRadius,
+                                   outerRadius,
+                                   percent,
+                                   index
+                               }: any) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
+    return (
+        <text
+            x={x}
+            y={y}
+            fill="white"
+            textAnchor={x > cx ? "start" : "end"}
+            dominantBaseline="central"
+        >
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
 };
 
-function CategoryPieChart() {
+export default function CategoryPieChart() {
     return (
-        <ResponsiveContainer width="100%" height="100%">
-            <PieChart width={400} height={400}>
-                <Pie
+        <PieChart width={400} height={400}>
+            <Pie
                 data={data}
-                cx="50%"
-                cy="50%"
+                cx={200}
+                cy={200}
                 labelLine={false}
                 label={renderCustomizedLabel}
                 outerRadius={80}
                 fill="#8884d8"
                 dataKey="value"
-                >
+            >
                 {data.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
-                </Pie>
-            </PieChart>
-        </ResponsiveContainer>
-    )
+            </Pie>
+        </PieChart>
+    );
 }
-
-export default CategoryPieChart;
